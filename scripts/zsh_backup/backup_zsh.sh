b511ba9605d5df77403d71e556e56bf69f14ebbf
Rohan Chakravarthy <rohanc219@gmail.com>
2017-04-22 16:31:58 -0700

    zsh backup stuff
    README

---
+++ scripts/zsh_backup/backup_zsh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pushd $HOME/code/personal/secrets
+cp $HOME/.zsh_hist_backup.db ./backups/.zsh_hist_backup.db
+current_date=$(date)
+git add ./backups/.zsh_hist_backup.db
+git commit -m "zsh-backup-$current_date"
+git push origin master
+popd
+
